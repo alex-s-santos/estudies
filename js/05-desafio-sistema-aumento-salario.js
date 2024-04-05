ccc3722a7486c802a1600c59b7a7b02c542c3c8a
Alex Santos <alexsantos.alss.13@gmail.com>
2024-04-05 17:13:12 -0300

    Iniciando repositório

---
+++ js/05-desafio-sistema-aumento-salario.js
@@ -0,0 +1,17 @@
+//Desafio aumento de salário (a cima de 2000 aumento de 6% de salario e vale alimentação, a baixo 10% e vale alimentação) - Salário família 3% por dependente, vale refeição 60% do salário. Mostrar dados no final.
+
+let salarioAtual = 2000;
+let quantidadeDeDependetes = 3;
+let valeRefeicao = salarioAtual * 0.6;
+let aumentoSalario;
+let aumentoValeAlimentacao;
+let novoSalario;
+let novo
+let totalSalario;
+
+if(salarioAtual > 2000) {
+    aumentoSalario = salarioAtual + 0.06;
+
+} else {
+
+}
